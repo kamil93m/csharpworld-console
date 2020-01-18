fddc3bc0a107a1e44a78713a5653fc7357ca1539
Kamil <k4mil_92@o2.pl>
2020-01-18 22:18:20 +0100

    Implemented simple switch-case menu

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,27 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! Press any key to close application...");
+            Console.WriteLine("This is HelloWorld application. Application allows to read data to file, and write them to file.");
+            
+            switch (Console.ReadLine())
+            {
+                case "read":
+                    Console.WriteLine("Working in progress... this function coming soon.");
+                    break;
+
+                case "write":
+                    using (System.IO.StreamWriter file =
+                        new System.IO.StreamWriter(@"C:\Users\Kamil\Desktop\WriteLines2.txt", true))
+                    {
+                        file.WriteLine("Fourth line");
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid option, application will be closed...");
+                    break;
+            }
+            Console.WriteLine("Press any key to close application...");
             Console.ReadKey();
         }
     }
