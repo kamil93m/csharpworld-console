416f7dcb3ec092f737950ab5bf7d280d2ea08c6d
Kamil <k4mil_92@o2.pl>
2020-01-18 21:46:21 +0100

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Press any key to close application...");
+            Console.ReadKey();
+        }
+    }
+}
